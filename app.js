77c171176c842fb0ee8a54a5ba872a37d6f4ebb8
EdizYegenoglu <Ediz.yegenoglu@hva.nl>
2021-05-05 13:47:58 +0200

    Bij de app.post/order word een id als string  meegegeven die matched met de _id van de order zodat je na het klikken van afrekenen een nieuwe order kan beginnen

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const bodyParser = require('body-parser');
 const { CLIENT_RENEG_WINDOW } = require('tls');
 const mongoose = require('mongoose');
 
-
 // static files
 app.use(express.static(`${__dirname}static`));
 app.use('/static', express.static(path.join(__dirname, '/static')));
@@ -42,45 +41,41 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-
 app.get('/', async (req, res) => {
 	const sausjes = await db.collection('extra').find().toArray();
 	const products = await db.collection('products').find().toArray();
 	const productCategories = await db.collection('product-categories').find().toArray();
-	// const order_products = await db.collection('order-products').find().toArray();
-
-	const orders = await db.collection('orders').aggregate([
-		{ 
-			$match : { 
-				done : 0 
-			} 
-		},
-		{
-			$lookup: {
-				from: 'order-products',
-				localField: 'id',
-				foreignField: 'order_id',
-				as: 'order_products'
-			}
-		},
-		{
-			$unwind: {
-				"path": '$products',
-				"preserveNullAndEmptyArrays": true
-			}
-		},
-		{
-			$lookup: {
-				from: 'products',
-				localField: 'products.order_products.product_id',
-				foreignField: 'id',
-				as: 'products'
-			}
-		},
-		// { $convert: { input: '6086bce5df1c7f6e50e0495e', to: "objectId" } },
-
-		]).toArray();
-
+	const orders = await db.collection('orders').aggregate(
+		[
+			{ 
+				$match : { 
+					done : 0 
+				} 
+			},
+			{
+				$lookup: {
+					from: 'order-products',
+					localField: 'id',
+					foreignField: 'order_id',
+					as: 'order_products'
+				}
+			},
+			{
+				$unwind: {
+					"path": '$products',
+					"preserveNullAndEmptyArrays": true
+				}
+			},
+			{
+				$lookup: {
+					from: 'products',
+					localField: 'products.order_products.product_id',
+					foreignField: 'id',
+					as: 'products'
+				}
+			},
+		]
+	).toArray();
 	const categories = await db.collection('categories').aggregate([
 		{
 			$lookup: {
@@ -105,43 +100,38 @@ app.get('/', async (req, res) => {
 			}
 		}
 	]).toArray();
-	// const combined = products.concat(order_products)
-
-	console.log(orders);
-	
-	//console.log(categories);
-
-	// const totalPrice = await db.collection('order-products').aggregate({
-	// 	$group: {
-	// 		_id: '',
-	// 		totalAmount: { $sum: '$total' }
-	// 	}
-	//  }, {
-	// 	$project: {
-	// 		_id: 0
-	// 	}
-	// })
-	// db.collection('order-products').aggregate({
-	// 	$convert: { input: "product_id", to: "ObjectId"}
-	// })
+	const totalPrice = await db.collection('order-products').aggregate({
+		$group: {
+			_id: '',
+			totalAmount: { $sum: '$' }
+		}
+	 }, {
+		$project: {
+			_id: 0
+		}
+	})
+	db.collection('order-products').aggregate({
+		$convert: { input: "product_id", to: "ObjectId"}
+	})
     res.render('index', {
 		sausjes: sausjes,
 		products: products,
 		productCategories: productCategories,
 		categories: categories,
 		order: orders[0],
-		// total: totalPrice
+		
+		total: totalPrice
 	})	
-
-	// console.log(orders.order_products);
 });
+
   app.post('/add', (req, res) => {
 	db.collection('order-products').insertOne(req.body, (err, result) => {
 	  if (err) return console.log(err)
-	  console.log(req.body, {_id: ObjectId(req.params.id)})
+	//   console.log(req.body, {_id: ObjectId(req.params.id)})
 	  res.redirect('/')
 	})
   })
+
   app.post('/delete/:id', (req, res) => {
 	db.collection('order-products').deleteOne({_id: ObjectId(req.params.id)}, (err, result) => {
 	  if (err) return console.log(err)
@@ -149,32 +139,25 @@ app.get('/', async (req, res) => {
 	})
   })
 
-  app.post('/order', (req, res) => {
+  app.post('/order', async (req, res) => {
 	db.collection('orders').updateOne({
-		done: 0
+		done: 0,
 	},
 	{$set:{ done: 1}
   })
-	db.collection('orders').insertOne({
-		// id: db.collection('orders').find().sort({"_id" : -1}).limit(1),
-		// id: db.collection('orders')._id,
+	var afrekenen =  await db.collection('orders').insertOne({
+		id: 0,
 		done: 0,
 		paid: 0
 	})
+	var afrekenen = db.collection('orders').updateOne({
+		id: 0,
+	},
+	{$set:{ id: afrekenen.insertedId.toString()}
+  })
 	  res.redirect('/')
-	  console.log(id)
   })
 
-//   const updateStuff = (id, doc) => {
-// 	  if (!ObjectId.isValid(s)) {
-// 		  return Promise.reject(new TypeError(`Invalid id: ${id}`));
-// 	  }
-// 	  return collection('order-products').findOneAndUpdate(
-// 		  {_id: ObjectId(id)},
-// 		  {$set: doc},
-// 		  {returnOriginal: false}
-// 	  )
-//   }
 app.use((req, res) => {
 	res.status(404).send('this page does not exist.');
 });
