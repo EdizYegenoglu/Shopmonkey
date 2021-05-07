cc7200ccc6a0b3ef58c7deb7afaa12f138acc279
EdizYegenoglu <Ediz.yegenoglu@hva.nl>
2021-05-07 13:43:31 +0200

    saus optie veranderd van checkbox naar radiobutton.

--- static/script/productChoices.js
+++ static/script/productChoices.js
@@ -1,13 +1,13 @@
-const inputBox = document.querySelectorAll('fieldset:nth-of-type(2) input');
-const checkBox = document.querySelectorAll('form fieldset:nth-of-type(2) div label');
+// const inputBox = document.querySelectorAll('fieldset:nth-of-type(2) input');
+// const checkBox = document.querySelectorAll('form fieldset:nth-of-type(2) div label');
 
-checkBox.forEach((checkBox, index) => {
-    checkBox.addEventListener('click', () => {
-        if(inputBox[index].checked == true){
-            checkBox.classList.add('selected')
-        }
-        else{
-            checkBox.classList.remove('selected')
-        }
-    })
-});
+// checkBox.forEach((checkBox, index) => {
+//     checkBox.addEventListener('click', () => {
+//         if(inputBox[index].checked == true){
+//             checkBox.classList.add('selected')
+//         }
+//         else{
+//             checkBox.classList.remove('selected')
+//         }
+//     })
+// });
